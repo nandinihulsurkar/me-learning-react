0e544ae3b9fe1537c017769e85120acdf38947c3
AJ Malgar <nandini@erisn.com>
2023-10-03 23:29:08 +0530

    creating repo

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+    "h1",
+    {id:"heading", class:"its_h1"},
+    "Namastey React from react!!"
+);
+//NOTE: heading here is just a normal javascript object which is created using React.createElement
+
+const root = ReactDOM.createRoot(document.getElementById("main_div"));
+
+root.render(heading);
+/* render method is responsible for converting the JS object heading
+into h1 tag which the browser understands and
+puts it in the specified place */
+
+
+/*const nestedDivStr = React.createElement(
+    "div",{id:"first_div"},"hi i am the div here"
+);
+const nestedroot = ReactDOM.createRoot(document.getElementById("nested_div"));
+nestedroot.render(nestedDivStr);*/
---
+++ README.md
@@ -0,0 +1,1 @@
+# this is a react learning process for me

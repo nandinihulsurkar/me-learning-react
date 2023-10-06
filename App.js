e16c929e510a83e2ea8c650cfde743aff9fda51a
AJ Malgar <nandinihulsurkar@gmail.com>
2023-10-06 16:55:51 +0530

    commiting the work of chapter 3 igniting the app

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading", className:"its_h1"},
-    "Namastey React from react"
+const itsMe = <span> This is Nandini Hulsurkar</span>;
+const jsxHeading1 = (
+    <h1 id='jsx_heading'>Namastey React : {itsMe} 👌</h1>
 );
-//NOTE: heading here is just a normal javascript object which is created using React.createElement
+
+function TheHeader(){
+    return (
+    <div id='header_container'>
+        <div id='left_header' className="">
+            Nandini Hulsurkar
+        </div>
+        <div id="right_header" className="">
+            HI.. This is nandini hulsurkar, welcomes you to my react learning place.
+        </div>
+    </div>
+    );
+}
+
+function  TheFooter(){
+    return (
+        <div id="footer_container">
+            The footer content will come here.
+        </div>
+    );
+}
+
+const TheIndexContent = () => (
+    <div>        
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+        The index page content goes here. The index page content goes here. The index page content goes here. <br/>
+    </div>
+);
+
+const TheCompleteIndexPage = () => (
+    <div>
+        <TheHeader /><br/>
+        <TheIndexContent /><br/>
+        
+        <TheFooter />
+        <TheFooter></TheFooter>
+        {TheFooter()}
+    </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("main_div"));
-
-root.render(heading);
-/* render method is responsible for converting the JS object heading
-into h1 tag which the browser understands and
-puts it in the specified place */
-
-
-/*const nestedDivStr = React.createElement(
-    "div",{id:"first_div"},"hi i am the div here"
-);
-const nestedroot = ReactDOM.createRoot(document.getElementById("nested_div"));
-nestedroot.render(nestedDivStr);*/
+root.render(<TheCompleteIndexPage />);
